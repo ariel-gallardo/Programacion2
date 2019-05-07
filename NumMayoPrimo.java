2a8c82537beba9a80e2a097fdbeca2489f3a817c
ariel-gallardo <38444032+ariel-gallardo@users.noreply.github.com>
2019-05-06 23:47:43 -0300

    Update NumMayoPrimo.java

--- NumMayoPrimo.java
+++ NumMayoPrimo.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,5 +13,37 @@
  */
 public class NumMayoPrimo
 {
-    
+    private static int arreglo[];
+
+    public static void main(String[] args)
+    {
+	arreglo = new int[10];
+        llenarManual();
+        System.out.println("posicion de numero Mayor: "+posMayor());
+    }
+    public static void llenarManual()
+    {
+        Scanner sC = new Scanner(System.in);
+        for (int i = 0; i < arreglo.length; i++)
+        {
+            arreglo[i] = sC.nextInt();
+        }
+    }
+
+    public static int posMayor()
+    {
+        int resultado = 0;
+        int Mayor = arreglo[0];
+        
+        for (int i = 0; i < arreglo.length; i++)
+        {
+            if(arreglo[i] > Mayor)
+            {
+                    Mayor = arreglo[i];
+                resultado = i;
+            }
+        }
+        return resultado;
+    }
 }
+
