ab638e31ea875e8610301de4c9f62e5c6078fc6b
ariel-gallardo <38444032+ariel-gallardo@users.noreply.github.com>
2019-05-06 23:49:24 -0300

    Update NumTerminados4.java

--- NumTerminados4.java
+++ NumTerminados4.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,5 +13,36 @@
  */
 public class NumTerminados4
 {
-    
+    private static int arreglo[];
+
+    public static void main(String[] args)
+    {
+	arreglo = new int[10];
+        llenarManual();
+	terminadoen4();
+    }
+
+    public static void llenarManual()
+    {
+        Scanner sC = new Scanner(System.in);
+        for (int i = 0; i < arreglo.length; i++)
+        {
+            arreglo[i] = sC.nextInt();
+        }
+    }
+        
+    public static void terminadoen4()
+    {
+        for (Integer i = 0; i < arreglo.length; i++)
+        {
+            String aCadena = String.valueOf(arreglo[i]);
+            if(aCadena.charAt(aCadena.length()-1) == '4')
+            {
+                System.out.println("Posicion "+i+" numero "+arreglo[i]);
+            }
+            
+        }
+    }
+
+   
 }
